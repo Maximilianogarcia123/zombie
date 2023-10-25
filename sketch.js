d0ca58bc94701fc680870308dd9ca999f4256ba4
Maximilianogarcia123 <133285310+Maximilianogarcia123@users.noreply.github.com>
2023-10-24 19:27:35 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,72 +1,137 @@
-var bg,bgImg;//bg es el tamaño de el espacio o background is bgimg es el fondo como las lapidas.
-var player, shooterImg, shooter_shooting;
-var zombie,zombie2,bala;
-var zombieImg,zombie2Img,balaImg;
+var score =0;
+var gun,bluebubble,redbubble, bullet, backBoard;
+
+var gunImg,bubbleImg, bulletImg, blastImg, backBoardImg;
+
+var redBubbleGroup, redBubbleGroup, bulletGroup;
+
+
+var life =3;
+var score=0;
+var gameState=1
 
 function preload(){
-  
-  shooterImg = loadImage("assets/shooter_2.png")
-  shooter_shooting = loadImage("assets/shooter_3.png")
-  bgImg = loadImage("assets/bg.jpeg")
-  zombieImg= loadImage("assets/zombie.png")
-  zombie2Img= loadImage("assets/zombie2.jpg")
-  balaImg= loadImage("assets/bala.jpg")
+  gunImg = loadImage("gun1.png")
+ blastImg = loadImage("blast.png")
+  bulletImg = loadImage("bullet1.png")
+  blueBubbleImg = loadImage("waterBubble.png")
+  redBubbleImg = loadImage("redbubble.png")
+  backBoardImg= loadImage("back.jpg")
 }
-
 function setup() {
+  createCanvas(800, 600);
 
+  backBoard= createSprite(50, width/2, 100,height);
+  backBoard.addImage(backBoardImg)
   
-  createCanvas(windowWidth,windowHeight);
-
-  //Añadir la imagen de fondo
-  bg = createSprite(displayWidth/2-20,displayHeight/2-40,20,20)
-bg.addImage(bgImg)
-bg.scale = 1.1
+  gun= createSprite(100, height/2, 50,50);
+  gun.addImage(gunImg)
+  gun.scale=0.2
   
-
-//Crear el sprite del jugador
-player = createSprite(displayWidth-1150, displayHeight-300, 50, 50);
- player.addImage(shooterImg)
-   player.scale = 0.3
-   player.debug = true
-   player.setCollider("rectangle",0,0,300,300)
-
-zombie = createSprite(displayWidth-300, displayHeight-250, 50, 50);
-zombie.addImage(zombieImg)
-zombie.scale=0.2;
-
-zombie2= createSprite(displayWidth-100, displayHeight-250, 50, 50);
-zombie2.addImage(zombie2Img)
-zombie2.scale=1;
+  bulletGroup = createGroup();   
+  blueBubbleGroup = createGroup();   
+  redBubbleGroup = createGroup();   
+  
+  heading= createElement("h1");
+  scoreboard= createElement("h1");
 }
 
 function draw() {
-  background(0); 
+  background("#BDA297");
+  
+  heading.html("Vida: "+life)
+  heading.style('color:red'); 
+  heading.position(150,20)
 
+  scoreboard.html("Puntuación: "+score)
+  scoreboard.style('color:red'); 
+  scoreboard.position(width-200,20)
 
+  if(gameState===1){
+    gun.y=mouseY  
 
+    if (frameCount % 80 === 0) {
+      drawblueBubble();
+    }
 
-  //Mover al jugador arriba y abajo y hacer que el juego móvil sea compatible de forma táctil.
-if(keyDown("UP_ARROW")||touches.length>0){
-  player.y = player.y-30
+    if (frameCount % 100 === 0) {
+      drawredBubble();
+    }
+
+    if(keyDown("space")){
+      shootBullet();
+    }
+
+    if (blueBubbleGroup.collide(backBoard)){
+      handleGameover(blueBubbleGroup);
+    }
+    if (redBubbleGroup.collide(backBoard)) {
+      handleGameover(redBubbleGroup);
+    }
+    
+    if(blueBubbleGroup.collide(bulletGroup)){
+      handleBubbleCollision(blueBubbleGroup);
+    }
+
+    if(redBubbleGroup.collide(bulletGroup)){
+      handleBubbleCollision(redBubbleGroup);
+    }
+
+    drawSprites();
+  }
+    
+  
 }
-if(keyDown("DOWN_ARROW")||touches.length>0){
- player.y = player.y+30
+
+function drawblueBubble(){
+  bluebubble = createSprite(800,random(20,780),40,40);
+  bluebubble.addImage(blueBubbleImg);
+  bluebubble.scale = 0.1;
+  bluebubble.velocityX = -8;
+  bluebubble.lifetime = 400;
+  blueBubbleGroup.add(bluebubble);
+}
+function drawredBubble(){
+  redbubble = createSprite(800,random(20,780),40,40);
+  redbubble.addImage(redBubbleImg);
+  redbubble.scale = 0.1;
+  redbubble.velocityX = -1;
+  redbubble.lifetime = 400;
+  redBubbleGroup.add(redbubble);
 }
 
-
-//Liberar las balas y cambiar la imagen del tirador a la posición de disparo cuando la barra espaciadora se presiona.
-if(keyWentDown("space")){
- 
-  player.addImage(shooter_shooting)
- 
+function shootBullet(){
+  bullet= createSprite(150, width/2, 50,20)
+  bullet.y= gun.y-20
+  bullet.addImage(bulletImg)
+  bullet.scale=0.12
+  bullet.velocityX= 7
+  bulletGroup.add(bullet)
 }
 
-//El jugador regresa a la imagen de posición original una vez que dejamos de presionar la barra de espacio.
-else if(keyWentUp("space")){
-  player.addImage(shooterImg)
+function handleBubbleCollision(bubbleGroup){
+    if (life > 0) {
+       score=score+1;
+    }
+
+   blast= createSprite(bullet.x+60, bullet.y, 50,50);
+  blast.addImage(blastImg)
+   blast.scale=0.3
+ blast.life=20
+    bulletGroup.destroyEach()
+    bubbleGroup.destroyEach()
 }
 
-drawSprites();
+function handleGameover(bubbleGroup){
+  
+    life=life-1;
+    bubbleGroup.destroyEach();
+    
 
+    if (life === 0) {
+      gameState=2
+      
+     
+    }
+  
 }
